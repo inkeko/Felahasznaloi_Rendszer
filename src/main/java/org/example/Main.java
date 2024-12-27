4ceacfa680ef48840afe2152e595a73b2ec81b47
Földi Csaba <inkeko@gmail.com>
2024-12-27 11:26:42 +0100

    repair code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,11 @@
 package org.example;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/user_view.fxml"));
-        Parent root = loader.load();
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+public class Main {
 
-    }
 
     public static void main(String[] args) {
-        launch(args);
+       App.launch(App.class, args);
     }
 }
